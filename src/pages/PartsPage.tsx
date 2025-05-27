3415998d808e8c5101f6b6e5bfb3f33360add92f
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-05-28 00:36:06 +0300

    made it 2 columns for mobile

--- src/pages/PartsPage.tsx
+++ src/pages/PartsPage.tsx
@@ -158,7 +158,7 @@ const PartsPage: React.FC = () => {
               </button>
             </div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
               {filteredParts.map((part) => (
                 <PartCard
                   key={part.id}
